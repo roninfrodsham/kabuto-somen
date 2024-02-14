cff764ff8b7c997f6cd2269d52d9c50df1b0c57b
roninfrodsham <roninfrodsham@gmail.com>
2024-02-14 09:24:02 +0000

    upgrade packages and update any on type

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
-import { motion } from 'framer-motion'
-import { HelmetBack } from './components/helmetback'
-import { HelmetFront } from './components/helmetfront'
-import { MaskOne, MaskTwo, MaskThree, MaskFour } from './components/masks'
-import './App.css'
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { HelmetBack } from "./components/helmetback";
+import { HelmetFront } from "./components/helmetfront";
+import { MaskOne, MaskTwo, MaskThree, MaskFour } from "./components/masks";
+import "./App.css";
 
 type ComponentProps = {
-  [key: string]: any
-}
+  [key: string]: JSX.Element;
+};
 
 function App() {
   const Components: ComponentProps = {
-    "MaskOne": MaskOne,
-    "MaskTwo": MaskTwo,
-    "MaskThree": MaskThree,
-    "MaskFour": MaskFour,
-  }
-  const keys = Object.keys(Components)
-  const [currentMask, setMask] = useState("MaskOne")
+    MaskOne: MaskOne,
+    MaskTwo: MaskTwo,
+    MaskThree: MaskThree,
+    MaskFour: MaskFour,
+  };
+  const keys = Object.keys(Components);
+  const [currentMask, setMask] = useState("MaskOne");
   let Component = Components[currentMask];
 
   const helmetVariants = {
@@ -28,19 +28,19 @@ function App() {
       opacity: 1,
       transition: {
         duration: 1,
-        type: 'tween',
-        ease: 'easeInOut',
-        when: 'beforeChildren',
-      }
+        type: "tween",
+        ease: "easeInOut",
+        when: "beforeChildren",
+      },
     },
-  }
+  };
 
   function getRandomMask() {
     const filterKeys = keys.filter((key) => {
-      return key !== currentMask
-    })
-    console.log("FILTER KEYS: ", filterKeys);
-    const prop = filterKeys[Math.floor(Math.random() * filterKeys.length)]
+      return key !== currentMask;
+    });
+
+    const prop = filterKeys[Math.floor(Math.random() * filterKeys.length)];
     setMask(prop);
   }
 
@@ -50,43 +50,45 @@ function App() {
         baseProfile='tiny'
         overflow='visible'
         viewBox='0 0 332.6 275.1'
-        className='kabuto' 
+        className='kabuto'
         variants={helmetVariants}
         initial='hidden'
-        animate={'visible'}
+        animate={"visible"}
       >
         <HelmetBack />
-        <Component maskVariants={{
-          hidden: {
-            opacity: 0,
-            y: -50
-          },
-          visible: {
-            opacity: 1,
-            y: 0,
-            transition: {
-              type: 'spring',
-              stiffness: 160,
-              damping: 12,
+        <Component
+          maskVariants={{
+            hidden: {
+              opacity: 0,
+              y: -50,
             },
-            default: {
-              duration: 0.5,
-            }
-          }
-        }} />
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: "spring",
+                stiffness: 160,
+                damping: 12,
+              },
+              default: {
+                duration: 0.5,
+              },
+            },
+          }}
+        />
         <HelmetFront />
       </motion.svg>
       <motion.button
         onClick={() => getRandomMask()}
         whileHover={{
           scale: 1.2,
-          textShadow: '0 0 10px #EEABCE',
+          textShadow: "0 0 10px #EEABCE",
         }}
       >
         Change Mask
       </motion.button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
